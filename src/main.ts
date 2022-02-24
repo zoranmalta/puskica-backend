763d66b57028a8ce81a911dec149b5da3c27956d
zoranmalta <zoranmarcetic2@gmail.com>
2022-02-25 00:05:37 +0100

    init

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app
+    .useGlobalPipes(
+      new ValidationPipe({
+        //  automatically transform payloads to be objects typed according to their DTO classes.
+        transform: true,
+        // any property not included in the whitelist is automatically stripped from the resulting object
+        //property can be automatically removed from the resulting DTO.
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    )
+    .enableCors();
+  await app.listen(3000);
+}
+bootstrap();
